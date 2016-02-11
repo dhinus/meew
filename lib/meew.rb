fb69cf6eb9b2b5934be098af045e31d7838ca9ba
Francesco Negri <francesco.negri@bbc.co.uk>
2016-02-11 00:43:58 +0000

    Scaffolding for commands

--- lib/meew.rb
+++ lib/meew.rb
@@ -1,5 +1,26 @@
 class Meew
-  def self.run(command = '')
-    puts "Meeeeew! #{command}"
+  def self.run(args)
+    case args[0]
+    when "browser"
+      not_implemented
+    when "file"
+      not_implemented
+    when "fact"
+      not_implemented
+    else
+      print_usage
+    end
+  end
+
+  def self.print_usage
+    puts "\nUsage: meew [command]"
+    puts "\nAvailable commands:"
+    puts "  browser    open a new browser window displaying an image of a cat"
+    puts "  file       save on the desktop an image of a cat"
+    puts "  fact       display a cat fact"
+  end
+
+  def self.not_implemented
+    puts "\nThis command is not implemented yet"
   end
 end
--- spec/meew_spec.rb
+++ spec/meew_spec.rb
@@ -1,8 +1,38 @@
 require 'meew'
 
 RSpec.describe Meew, "#run" do
-  it "outputs Meeeeew!" do
-    expect{ Meew.run }.
-      to output(/Meeeeew!/).to_stdout
+  context "if no command is specified" do
+    it "shows usage information" do
+      expect { Meew.run([]) }.
+        to output(/Usage: meew \[command\]/).to_stdout
+    end
+  end
+
+  context "if an invalid command is specified" do
+    it "shows usage information" do
+      expect { Meew.run(["dog"]) }.
+        to output(/Usage: meew \[command\]/).to_stdout
+    end
+  end
+
+  context "'browser' command" do
+    it "shows 'not implemented'" do
+      expect { Meew.run(["browser"]) }.
+        to output(/This command is not implemented yet/).to_stdout
+    end
+  end
+
+  context "'file' command" do
+    it "shows 'not implemented'" do
+      expect { Meew.run(["browser"]) }.
+        to output(/This command is not implemented yet/).to_stdout
+    end
+  end
+
+  context "'fact' command" do
+    it "shows 'not implemented'" do
+      expect { Meew.run(["browser"]) }.
+        to output(/This command is not implemented yet/).to_stdout
+    end
   end
 end
